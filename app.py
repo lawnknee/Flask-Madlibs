ba4a64bc9cd14aec4c847ee727d98d9915b58e68
Loni <kuangloni@gmail.com>
2021-07-07 16:02:37 -0700

    app works

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def homepage():
+    """Shows the homepage with the Madlib prompt form"""
+
+    prompt_list = silly_story.prompts
+
+    return render_template("questions.html", prompts=prompt_list)
+
+@app.route('/results')
+def result_story():
+    """Creates a dictionary of prompt and user's answers
+        Generates a story. 
+    """
+
+    ans = request.args
+
+    result_story = silly_story.generate(ans)
+
+    return render_template("story.html", story=result_story)
+
+    # request.args is dictionary-like
