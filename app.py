65c78a57c028cac55f700217c19d0d3c16e5b849
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-07-07 17:11:13 -0700

    further study completed

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+STORIES = {"silly" : silly_story, "excited" : excited_story}
+
 @app.route('/')
 def homepage():
-    """Shows the homepage with the Madlib prompt form"""
+    """Shows homepage where you can choose which story"""
+    return render_template("choose.html", stories = ["silly", "excited"])
 
-    prompt_list = silly_story.prompts
 
-    return render_template("questions.html", prompts=prompt_list)
+@app.route('/madlibs')
+def madlibs():
+    """Shows the Madlib prompt form"""
 
-@app.route('/results')
-def result_story():
+    selected_story = request.args["story"]
+    prompt_list = STORIES[selected_story].prompts
+
+    return render_template("questions.html", prompts = prompt_list, selected_story = selected_story)
+
+@app.route('/results/<selected_story>')
+def result_story(selected_story):
     """Creates a dictionary of prompt and user's answers
         Generates a story. 
     """
 
     ans = request.args
 
-    result_story = silly_story.generate(ans)
+    result_story = STORIES[selected_story].generate(ans)
 
     return render_template("story.html", story=result_story)
 
